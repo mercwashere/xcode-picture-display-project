3a8d0b76924c91c2e18744bd4db4334dce523521
mercwashere <ambikatalwar0405@gmail.com>
2026-02-19 10:40:09 +0000

    Initial Commit

---
+++ my-very-cool-app/my_very_cool_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_very_cool_appApp.swift
+//  my-very-cool-app
+//
+//  Created by Ambika   on 19/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct my_very_cool_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
